3ffdd6e6e2811163df287ab10d4cbfb661787c2f
Gary Dix <intelproof@gmail.com>
2025-05-12 11:44:46 +1200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VisitorManagement2025.Models;
+
+namespace VisitorManagement2025.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<VisitorManagement2025.Models.StaffNames> StaffNames { get; set; } = default!;
+        public DbSet<VisitorManagement2025.Models.Visitors> Visitors { get; set; } = default!;
+    }
+}
---
+++ Models/Visitors.cs
@@ -0,0 +1,17 @@
+﻿namespace VisitorManagement2025.Models
+{
+    public class Visitors
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Business { get; set; }
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+        public Guid StaffNameId { get; set; }
+        public StaffNames? StaffName { get; set; }
+
+
+
+    }
+}
