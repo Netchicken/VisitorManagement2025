7d9d5f8504ca5091815e52c7d6a8f3329a358ae5
Gary Dix <intelproof@gmail.com>
2025-05-12 12:09:39 +1200

    DB created

--- Data/Migrations/20250512000341_Init.Designer.cs
+++ Data/Migrations/20250512000341_Init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VisitorManagement2025.Data;
 
@@ -11,9 +12,11 @@ using VisitorManagement2025.Data;
 namespace VisitorManagement2025.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250512000341_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
